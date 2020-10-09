e551a91c34a64eb6382162ec8d35b5018907e0f7
californianseabass <releasetheseabass@gmail.com>
2020-10-09 12:04:52 -0700

    refactor lua config into separate file

---
+++ lisp/lua.el
@@ -0,0 +1,8 @@
+;;;; https://immerrr.github.io/lua-mode/
+(require-package 'lua-mode)
+
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(provide 'lua-init)
