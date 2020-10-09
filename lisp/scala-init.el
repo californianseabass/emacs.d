b47b17e013494aa5522d8bae2a50c624f2c6cff9
californianseabass <releasetheseabass@gmail.com>
2020-10-09 12:07:52 -0700

    wip. scala possibly broken

--- lisp/scala-init.el
+++ lisp/scala-init.el
@@ -1,5 +1,8 @@
 (require-package 'ammonite-term-repl)
+(require-package 'company-lsp)
+(require-package 'company-quickhelp)
 (require-package 'flycheck)
+(require-package 'sbt-mode)
 ;; Add metals backend for lsp-mode
 (require-package 'lsp-metals)
 ;; Enable nice rendering of documentation on hover
@@ -12,11 +15,29 @@
 ;;  ("scala" . scala-mode))
 
 
+(add-to-list 'auto-mode-alist '("\\.s\\(c\\|cala\\|bt\\)\\'" . scala-mode))
 
 (add-hook 'scala-mode-hook
           (lambda ()
             (ammonite-term-repl-minor-mode t)))
 
+(use-package lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook  (scala-mode . lsp)
+  (lsp-mode . lsp-lens-mode)
+  :config (setq lsp-prefer-flymake nil))
+
+
+;; add metals backend for lsp-mode
+(use-package lsp-metals)
+
+;; Enable nice rendering of documentation on hover
+(use-package lsp-ui)
+
+(use-package company-lsp)
+
+(use-package company-quickhelp
+  :hook (scala-mode . lsp))
 
 (add-hook 'scala-mode-hook #'flycheck-mode)
 
